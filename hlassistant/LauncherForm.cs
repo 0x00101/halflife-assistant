5f99678afa1ccea92d2f8da121d5575e85af97d5
Dmitry Yan <52624267+0x00101@users.noreply.github.com>
2019-08-08 00:11:53 +0400

    Some interface

---
+++ hlassistant/LauncherForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hlassistant
+{
+    public partial class LauncherForm : Form
+    {
+        public LauncherForm()
+        {
+            InitializeComponent();
+            if (!Directory.Exists("cstrike"))
+            {
+                cscbx.Enabled = false;
+            }
+
+            if (!Directory.Exists("ag"))
+            {
+                agcbx.Enabled = false;
+            }
+
+            if (!Directory.Exists("bshift"))
+            {
+                bscbx.Enabled = false;
+            }
+
+            if (!Directory.Exists("gearbox"))
+            {
+                ofcbx.Enabled = false;
+            }
+        }
+
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e) => MessageBox.Show(resolution.SelectedIndex.ToString());
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dccbx_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ hlassistant/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace hlassistant
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            if (!File.Exists("hl.exe"))
+            {
+                MessageBox.Show("Фатальная ошибка: Файл hl.exe не найден. Поместите лаунчер в корень игры.");
+                Term();
+            }
+
+            if (!Directory.Exists("valve"))
+            {
+                MessageBox.Show("Фатальная ошибка: Папка valve не найдена. Проверьте ваш дистрибутив игры.");
+                Term();
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new LauncherForm());
+        }
+
+        static void Term()
+        {
+            StreamReader sr = new StreamReader("debugmode.hla");
+            int debug = Int32.Parse(sr.ReadToEnd());
+            sr.Close();
+
+            if (debug == 0)
+            {
+                Environment.Exit(0);    
+            }
+        }
+
+    }
+}
