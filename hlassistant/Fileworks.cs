60b556b599c6a832db8a5ee9769aac83e9422265
Dmitry Yan <52624267+0x00101@users.noreply.github.com>
2019-08-08 14:26:39 +0400

    Log-system class added

---
+++ hlassistant/Fileworks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace hlassistant
+{
+    class Fileworks
+    {
+        public string ReadStr(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            return sr.ReadToEnd().ToString();
+        }
+
+        public int ReadInt(string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            return Int32.Parse(sr.ReadToEnd());
+        }
+    }
+}
---
+++ hlassistant/LogBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace hlassistant
+{
+    class LogBuilder
+    {
+        Fileworks fw = new Fileworks();
+        public void Init(string filename)
+        {
+            string version = fw.ReadStr("hla_base/version.txt");
+
+            FileStream aFile = new FileStream(filename, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(aFile);
+            aFile.Seek(0, SeekOrigin.End);
+            sw.WriteLine("Half-Life Assistant" + version);
+            sw.Close();
+        }
+    }
+}
